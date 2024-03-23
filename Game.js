9af2efc6975222a6205dc54946a4b7328894c739
Mohamed Tanash <tanashkoo@gmail.com>
2024-03-23 22:12:14 +0200

    Add zombie attack sound effect

--- Game.js
+++ Game.js
@@ -4,6 +4,7 @@ import enemyImage from "./images/tds_zombie/export/Attack/skeleton-attack_0.png"
 import playerImage from "./images/Top_Down_Survivor/shotgun/idle/survivor-idle_shotgun_0.png";
 import gunShotSound from "./sounds/gun-shot.mp3";
 import themeMusic from "./sounds/terror-ambience.mp3";
+import zombieAttackSound from "./sounds/Zombie-Attack.mp3";
 import zombieDeathSound from "./sounds/zombie-death.mp3";
 
 class GameScene extends Phaser.Scene {
@@ -19,6 +20,7 @@ class GameScene extends Phaser.Scene {
     this.load.audio("gunShot", gunShotSound);
     this.load.audio("zombieDeath", zombieDeathSound);
     this.load.audio("theme", themeMusic);
+    this.load.audio("zombieAttack", zombieAttackSound);
   }
 
   create() {
@@ -276,6 +278,10 @@ class GameScene extends Phaser.Scene {
 
       enemy.hit = true;
       enemy.setTint(0xff0000);
+
+      this.sound.play("zombieAttack", {
+        volume: 0.2,
+      });
 
       this.time.delayedCall(1000, () => {
         enemy.hit = false;
